5159c1e4e990501ce35e82d196155c3a826a4869
Nelly Sugu <jonly03@gmail.com>
2021-07-15 17:15:29 -0400

    Implements CRUD

---
+++ HELPERS/index.js
@@ -0,0 +1,17 @@
+function getRandomId() {
+  // returns a 4 character string
+  // every character is a random number between 0 - 9
+
+  let id = "";
+  for (let i = 0; i < 4; i++) {
+    let number = Math.floor(Math.random() * 10);
+
+    id += number;
+  }
+
+  return id;
+}
+
+module.exports = {
+  getRandomId,
+};
